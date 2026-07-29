1127e4ae0167fa00b28b71290ca6eb61f21f7a4a
user <user@example.com>
2026-07-29 17:10:13 +0700

    fix: revert @xmcl/windows-utils - external npm dep, not workspace

--- btnlauncher2-electron-app/main/utils/windowsUtils.ts
+++ btnlauncher2-electron-app/main/utils/windowsUtils.ts
@@ -4,9 +4,9 @@ import { Logger } from '@btnlauncher2/runtime/infra'
 export function getWindowsUtils(app: ElectronLauncherApp, logger: Logger) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const utils = require('@btnlauncher2/windows-utils')
+    const utils = require('@xmcl/windows-utils')
     logger.log('Success to load windows utils!')
-    return utils as typeof import('@btnlauncher2/windows-utils')
+    return utils as typeof import('@xmcl/windows-utils')
   } catch (e) {
     logger.warn('Fail to load windows utils!')
     return undefined
